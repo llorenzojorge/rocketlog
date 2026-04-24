b921216c32a56b416330a5dc18ae8bdd9db40b22
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2026-04-24 07:55:32 -0300

    Middleware de Tratamento de Exceções

---
+++ src/utils/AppError.ts
@@ -0,0 +1,13 @@
+import { stat } from "fs"
+
+class AppError {
+  message: string
+  statusCode: number
+
+  constructor(message: string, statusCode: number = 400) {
+    this.message = message
+    this.statusCode = statusCode
+  }
+}
+
+export { AppError }
