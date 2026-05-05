75a5ebfbfce936356411ce796e5941bc8393ce86
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2026-05-05 17:06:45 -0300

    Recuperando o Usuário

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,7 +1,34 @@
 import { Request, Response } from "express"
+import { prisma } from "@/database/prisma"
+import { z } from "zod"
+import { compare } from "bcrypt"
+import { AppError } from "@/utils/AppError"
 
 class SessionsController {
-  create(request: Request, response: Response) {
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      email: z.string().email(),
+      password: z.string().min(6),
+
+    })
+
+    const { email, password } = bodySchema.parse(request.body)
+
+    const user = await prisma.user.findFirst({
+      where: { email }
+    })
+
+    if (!user) {
+      throw new AppError("Invalid email or password", 401)
+    }
+
+    const passwordMatched = await compare(password, user.password)
+
+    if (!passwordMatched) {
+      throw new AppError("Invalid email or password", 401)
+    }
+
+
     return response.json({ message: "Ok" })
   }
 }
